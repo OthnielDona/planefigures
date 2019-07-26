cabfd2a651c76e96db531ceeeb40381a5cbc3dee
OthnielDona <othnieldona@gmail.com>
2019-07-26 20:58:16 +0000

    wrote setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="planefigures",
+    version="0.1.0",
+    author="Othniel Dona Monote",
+    author_email="othnieldona@gmail.com",
+    description="A turtle based package that makes drawing common geometric plane shapes a breeze.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OthnielDona/planefigures",
+    packages=["figures"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
