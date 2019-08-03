c8556434076430b51f33e9c406e90a2a5f97ff9a
OthnielDona <othnieldona@gmail.com>
2019-08-03 19:00:50 +0000

    updated righttriangle

--- planefigures/core.py
+++ planefigures/core.py
@@ -80,23 +80,32 @@ def parallelogram(length:int, breadth:int, angle:float):
 
 
 def righttriangle(a=None, b=None, c=None):
-    """[summary]
+    """Draw a right-angle triangle.
+    You can provide just two sides out of three or all three sides.
     
     Keyword Arguments:
-        a {[type]} -- [description] (default: {None})
-        b {[type]} -- [description] (default: {None})
-        c {[type]} -- [description] (default: {None})
+        a {float} -- the vertical side (default: {None})
+        b {float} -- the horizontal side (default: {None})
+        c {float} -- the hypotenus (default: {None})
     """
     
-    if a and b:
+    # Calculate  the third side from the two given sides
+    if a and b and not c:
         c = math.hypot(a,b)
-    elif a and c:
+    elif a and c and not b:
         b = math.sqrt(c**c - a**a)
-    elif b and c:
+    elif b and c and not a:
         a = math.sqrt(c**c - b**b)
+    elif a and b and c:
+        # Make sure the hypotenus c is the longest side
+        try:
+            assert math.hypot(a,b) is c
+        except AssertionError:
+            text = 'The three given sides do not satisfy the Pythagorean theorem.'
     else:
-        print('You must provide at least two out of the three sides.')
-
+        return print('You must provide at least two out of the three sides.')
+    
+    # Calculate angle A opposite side a
     x = math.degrees(math.asin(a/c))
 
     turtle.forward(b)
@@ -105,3 +114,12 @@ def righttriangle(a=None, b=None, c=None):
     turtle.left(90+x)
     turtle.forward(a)
     turtle.left(90)
+
+    if text:
+        turtle.penup()
+        # Print the text below the diagram
+        # Use c since it's the highest value of the diagram
+        turtle.goto(0,c+10)
+        turtle.write(text, align="center", font=("Arial",12,"bold"))
+        turtle.home()
+        turtle.pendown()
